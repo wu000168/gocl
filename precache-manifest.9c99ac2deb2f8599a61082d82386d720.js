ccc29cb1ef9ec7f272451d755d243ae6da50a613
wu000168 <wu000168@umn.edu>
2020-06-25 12:48:03 -0500

    Updates

---
+++ precache-manifest.9c99ac2deb2f8599a61082d82386d720.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00989dcd0e84bb99abee9aef0e8dcf1d",
+    "url": "/gocl/index.html"
+  },
+  {
+    "revision": "64b703d2f08ab0052663",
+    "url": "/gocl/static/css/main.67977a31.chunk.css"
+  },
+  {
+    "revision": "aedd133e391e324a0af4",
+    "url": "/gocl/static/js/2.918b700d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gocl/static/js/2.918b700d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64b703d2f08ab0052663",
+    "url": "/gocl/static/js/main.00d2dcbd.chunk.js"
+  },
+  {
+    "revision": "2719d57f9d1e1d1716aa",
+    "url": "/gocl/static/js/runtime-main.28e48f0e.js"
+  }
+]);
